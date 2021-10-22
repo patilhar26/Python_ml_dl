a0b3bfff4005a230345a899383e023c55c5f0ec9
patilhar26 <64339587+patilhar26@users.noreply.github.com>
2021-10-22 00:36:27 -0500

    commit

--- ICP1/ICP1-Answers/Source Code/Answer3.py
+++
@@ -1,7 +0,0 @@
-##-------------------------------------------------------------------------------------------------------------
-##  Student Name : Harshita Patil
-##  Code for Question 3
-
-#Replace each occurrence of 'python' with 'pythons'
-val = input("Enter a sentence: ")
-print(val.replace('python','pythons'))
